ed37ca897ff35b8b99e807edd5cc66ced9913649
Daniel <oscar.d.gonzalez@gmail.com>
2024-02-09 19:11:39 -0500

    feat: :mag: added AppConfig info for SEO

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,9 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Tech blog | Daniel González',
+  title: 'Tech blog | Daniel González',
+  description:
+    'Dive into coding insights: JavaScript, TypeScript, React & more. Stay ahead with my tech blog. Read now!',
+  author: 'Daniel González',
   locale_region: 'en-us',
   locale: 'en',
 };
